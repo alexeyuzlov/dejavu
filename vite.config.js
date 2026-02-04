626b2f00ae4f681ea723e0084197bb44ac411653
Alexey Uzlov <alexey.uzlov@workato.com>
2026-02-04 23:47:33 +0400

    Update script paths in index.html and enhance Vite configuration
    Refactor script paths in index.html to remove base URL placeholders for improved clarity. Update Vite configuration to define the project root, public directory, and output directory, ensuring a more structured build process with specified Rollup input options.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
 const { defineConfig } = require('vite');
 
-module.exports = defineConfig({
+module.exports = defineConfig(() => ({
+  root: '.',
+  publicDir: 'public',
+  base: '/dejavu/',
   define: {
     'import.meta.env.VITE_BUILD_DATE': JSON.stringify(new Date().toISOString()),
   },
-});
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
+}));
